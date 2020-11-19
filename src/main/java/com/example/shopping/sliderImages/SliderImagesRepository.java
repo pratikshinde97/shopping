1d360a10759afeb57bf3273ded28c1d974f52017
pratikshinde97 <pratikshinde972@gmail.com>
2020-11-19 17:27:34 +0530

    changes in slider images

--- src/main/java/com/example/shopping/sliderImages/SliderImagesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.shopping.sliderImages;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public interface SliderImagesRepository extends JpaRepository<SliderImages, String> {
-
-}
